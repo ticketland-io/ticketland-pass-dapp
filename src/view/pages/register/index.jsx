bf1ea92e856bc33707b21dd51dfa75d74e49713b
Antonis Poulakis <antwnic4@gmail.com>
2023-01-06 12:25:23 +0200

    feat: add content to register page

--- src/view/pages/register/index.jsx
+++ src/view/pages/register/index.jsx
@@ -1,9 +1,12 @@
 import React, {useContext, useEffect} from 'react'
 import {Context} from '../../core/Store'
 import {connectAccount} from '../../../services/account'
+import {useState} from 'react'
+import {Grid, Typography} from '@mui/material'
 
 const name = props => {
   const [state, _] = useContext(Context)
+  const [message, setMessage] = useState('Loading..')
 
   useEffect(() => {
     const urlSearchParams = new URLSearchParams(window.location.search)
@@ -13,21 +16,34 @@ const name = props => {
       if(currentUser) {
         const run = async () => {
           await connectAccount(state.firebase, qs['session_id'])
+          setMessage('Successfully connected your discord and ticketland accounts')
           window.location.href = `${process.env.DISCORD_LINK}/${qs.guild_id}/${qs.channel_id}`
         }
-  
+
         state.user && run()
-        .then(() => {})
-        .catch(error => console.log('>>>>>', error))
+          .then(() => { })
+          .catch(error => console.log('>>>>>', error))
       } else {
+        setMessage('Discord and ticketland accounts already connected')
         window.location.href = `${process.env.TICKETLAND_PASS_URL}/login?return-url=register?session_id=${qs['session_id']}&guild_id=${qs.guild_id}&channel_id=${qs.channel_id}`
       }
+
     })
   }, [state.user])
 
 
   return (
-    <div>Stateless Component</div>
+    <Grid
+      container
+      justifyContent='center'
+      alignItems='flex-start'
+      flexGrow={1}
+      mt='15%'
+    >
+      <Typography variant='h2'>
+        {message}
+      </Typography>
+    </Grid>
   )
 }
 

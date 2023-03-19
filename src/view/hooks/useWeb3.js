3d0ab1b677a735d88aed206b3a80cb8b7c66847f
ppoliani <ppoliani@gmail.com>
2023-03-19 12:59:22 +0200

    feat: update state and useWeb3

--- src/view/hooks/useWeb3.js
+++ src/view/hooks/useWeb3.js
@@ -1,10 +1,6 @@
 import {useEffect, useState, useContext} from 'react'
-import * as anchor from '@project-serum/anchor'
 import Web3 from '@apocentre/solana-web3'
 import {Context} from '../core/Store'
-
-const {PublicKey} = anchor.web3
-
 
 export default () => {
   const [web3, setWeb3] = useState(null)
@@ -14,12 +10,13 @@ export default () => {
     const initWeb3 = async () => {
       if(state.connection && state.user && !state.web3) {
         const _web3 = Web3()
-
+        let custodyWallet
+        
         if(state.walletType === 'custody') {
-          const custodyWallet = await state.eutopicCore.bootstrap(state.user)
-          await _web3.init(state.connection, custodyWallet)
+          custodyWallet = await state.walletCore.bootstrap()
         }
-
+        
+        await _web3.init(state.connection, custodyWallet)
         setWeb3(_web3)
       }
     }

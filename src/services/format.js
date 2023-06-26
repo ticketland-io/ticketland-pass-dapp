e7972e8f9264833849fd8e417d669523205aa0dd
George Niotis <geoniotis@yahoo.com>
2023-06-26 14:17:20 +0300

    feat: adding fromBase format function

--- src/services/format.js
+++ src/services/format.js
@@ -6,3 +6,9 @@ export const formatValue = (val, min = 2, max = 4) => new Intl.NumberFormat(
 export function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
+
+export const fromBase = (input, baseDecimals = 9) => {
+  const value = input / 10 ** baseDecimals
+
+  return value
+}
